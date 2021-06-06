bce854d8d8e8189ebfe8256e910aaed2184942ed
arun <arun.nimmoju1999@gmail.com>
2021-06-06 11:21:11 +0530

    order details UI added

--- app/src/main/java/com/example/agrigrass/OrderDetails.java
+++ app/src/main/java/com/example/agrigrass/OrderDetails.java
@@ -3,12 +3,23 @@ package com.example.agrigrass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class OrderDetails extends AppCompatActivity {
+
+
+    AutoCompleteTextView quantity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order_details);
+
+        quantity = findViewById(R.id.input_quantity);
+
+        String[] gender = new String[]{"1", "2","3","4","5","6","7","8","9","10"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_menu_popup_item, gender);
+        quantity.setAdapter(adapter);
     }
 }
